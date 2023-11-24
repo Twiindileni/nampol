c3521fc453a660b1a4f186952499f1a0a5c8f8e7
Twiindileni Tomas <150262434+Twiindileni@users.noreply.github.com>
2023-11-24 09:02:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,23 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
     loginForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        // User database with ID, Password, and Username
-        const userDatabase = {
-            "88032500588": { password: "1988", username: "John Doe" },
-            "1234": { password: "123", username: "Jane Doe" },
-            // Add more users as needed
-        };
-
+        // In a real application, you would make an AJAX request to the server for authentication
+        // For this example, we'll use a hardcoded username and ID for demonstration purposes
         const idNumber = document.getElementById("id-number").value;
         const password = document.getElementById("password").value;
 
-        // Check if the entered credentials match any user in the database
-        if (userDatabase.hasOwnProperty(idNumber) && userDatabase[idNumber].password === password) {
-            const username = userDatabase[idNumber].username;
-            alert(`Welcome, ${username}!`);
-
-            // Redirect to the dashboard page or perform other actions
-            // window.location.href = `fines.html`;
+        // Simulate server authentication (replace with your own authentication logic)
+        if (idNumber === "88032500588" && password === "1988") {
+            // Redirect to the dashboard page
+            window.location.href = `fines.html`;
         } else {
             alert("Invalid credentials. Please try again.");
         }
--- scriptofficerlogin.js
+++ scriptofficerlogin.js
@@ -5,23 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
 
         // In a real application, you would make an AJAX request to the server for authentication
-        // For this example, we'll use an object to store user credentials and redirect URLs
-        const userDatabase = {
-            "88032500588": { password: "1988", username: "Johannes Shilongo", redirectUrl: "List.html" },
-            "1234": { password: "123", username: "Jonas Donglass", redirectUrl: "List.html" },
-            // Add more users as needed
-        };
+        // For this example, we'll use an array to store user credentials and redirect URLs
+        const users = [
+            { idNumber: "88032500588", password: "1988", redirectUrl: "List.html" },
+            { idNumber: "12345", password: "123", redirectUrl: "List.html" },
+            // Add more users as needed with their corresponding redirect URLs
+        ];
 
         const idNumber = document.getElementById("id-number").value;
         const password = document.getElementById("password").value;
 
-        // Check if the entered credentials match any user in the database
-        if (userDatabase.hasOwnProperty(idNumber) && userDatabase[idNumber].password === password) {
-            const username = userDatabase[idNumber].username;
-            alert(`Welcome, ${username}`);
+        // Simulate server authentication by checking against the array
+        const user = users.find(u => u.idNumber === idNumber && u.password === password);
 
-            // Redirect to the specified URL after successful login
-            window.location.href = userDatabase[idNumber].redirectUrl;
+        if (user) {
+            // Redirect to the specified URL for the authenticated user
+            window.location.href = user.redirectUrl;
         } else {
             alert("Invalid credentials. Please try again.");
         }
